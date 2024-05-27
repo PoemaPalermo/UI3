cdb1f8385d758701bed96578d9f80d586f7a106e
PoemaPalermo <163904623+PoemaPalermo@users.noreply.github.com>
2024-05-27 15:51:07 -0300

    LogInManager_Script=Active

--- Assets/LogInMaager.cs
+++ Assets/LogInMaager.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+//usuario toca btnAceptar
+//Debug.Log("Por favor ingrese su contraseña");
+//if contraseña == inut
+//  Debug.Log("Access Granted");
+//else
+//  Debug.Log("Access Denied");
 public class LogInMaager : MonoBehaviour
 {
+    public InputField contraseña;
+    public string contraseñaDefualt;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,16 @@ public class LogInMaager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void botoncitoMagico()
+    {
+        if (contraseña.text == contraseñaDefualt)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
